480d0b6ead177074295e7cb42f889e2790ba0183
0x004d4m <0x004d4m@gmail.com>
2022-06-26 13:04:24 +0300

    updates

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -19,7 +19,7 @@ class Gallery extends Model
     ];
 
     public function getImageAttribute($value){
-        return url('public/'.$value);
+        return url($value);
     }
 
     public function setImageAttribute($value)

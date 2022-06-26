8360a681e8c8267fe4fb00ced9f66792ede98f3c
0x004d4m <0x004d4m@gmail.com>
2022-06-26 13:11:17 +0300

    updates

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -19,13 +19,13 @@ class Gallery extends Model
     ];
 
     public function getImageAttribute($value){
-        return asset('/'.$value);
+        return url($value);
     }
 
     public function setImageAttribute($value)
     {
         $attribute_name = "image";
-        $destination_path = "public/uploads/gallery";
+        $destination_path = "gallery";
 
         if ($value==null) {
             Storage::delete($this->{$attribute_name});
